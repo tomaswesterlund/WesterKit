750d58e7a7916e395342d9a24a3774af3d585189
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-02-26 12:07:53 -0600

    Update wk_app_colors.dart

--- lib/wk_app_colors.dart
+++ lib/wk_app_colors.dart
@@ -26,6 +26,18 @@ class WkAppColors {
   static const Color secondary700 = Color(0xFF00A343);
   static const Color secondary900 = Color(0xFF00702E);
 
+  // Grey
+  static const Color grey50  = Color(0xFFF9FAFB); // Extra light (Backgrounds)
+  static const Color grey100 = Color(0xFFF3F4F6); // Hover states
+  static const Color grey200 = Color(0xFFE5E7EB); // Borders / Dividers
+  static const Color grey300 = Color(0xFFD1D5DB); // Disabled elements
+  static const Color grey400 = Color(0xFF9CA3AF); // Hint text
+  static const Color grey500 = Color(0xFF6B7280); // Secondary text
+  static const Color grey600 = Color(0xFF4B5563); // Medium emphasis text
+  static const Color grey700 = Color(0xFF374151); // High emphasis text
+  static const Color grey800 = Color(0xFF1F2937); // Heading text
+  static const Color grey900 = Color(0xFF111827); // Near black
+
   // --- Status & Feedback ---
   static const Color info = Color(0xFF2196F3); // Blue
   static const Color danger = Color(0xFFD32F2F); // Red
