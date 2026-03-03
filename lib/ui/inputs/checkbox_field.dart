b39a0d1223b85cdb68ddfeecdd0630a50ff42e07
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-03 06:59:17 -0600

    Update checkbox_field.dart

--- lib/ui/inputs/checkbox_field.dart
+++ lib/ui/inputs/checkbox_field.dart
@@ -32,16 +32,14 @@ class CheckboxField extends StatelessWidget {
                 SizedBox(
                   height: 24,
                   width: 24,
-                  child: IgnorePointer(
-                    child: Checkbox(
-                      value: value,
-                      onChanged: (value) => onChanged(value),
-                      activeColor: colorScheme.primary,
-                      checkColor: colorScheme.onPrimary,
-                      side: BorderSide(color: colorScheme.outline, width: 1.5),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6),
-                      ),
+                  child: Checkbox(
+                    value: value,
+                    onChanged: (value) => onChanged(value),
+                    activeColor: colorScheme.primary,
+                    checkColor: colorScheme.onPrimary,
+                    side: BorderSide(color: colorScheme.outline, width: 1.5),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
                     ),
                   ),
                 ),
