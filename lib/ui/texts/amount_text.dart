81b3bc509c400e0e85907a12eb8a19a5667b0c5f
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-10 10:29:52 -0600

    Update amount_text.dart

--- lib/ui/texts/amount_text.dart
+++ lib/ui/texts/amount_text.dart
@@ -7,6 +7,7 @@ class AmountText extends StatelessWidget {
   final Color? color;
   final FontWeight fontWeight;
   final TextAlign? textAlign;
+  final double? amount; // Added to track raw value for conditional coloring
 
   const AmountText(
     this.text, {
@@ -14,6 +15,7 @@ class AmountText extends StatelessWidget {
     this.color,
     this.fontWeight = FontWeight.bold,
     this.textAlign,
+    this.amount,
     super.key,
   });
 
@@ -25,15 +27,15 @@ class AmountText extends StatelessWidget {
     FontWeight fontWeight = FontWeight.bold,
     TextAlign? textAlign,
     Key? key,
-  }) =>
-      AmountText(
-        CurrencyFormatter.fromDouble(amount),
-        key: key,
-        fontSize: fontSize,
-        color: color,
-        fontWeight: fontWeight,
-        textAlign: textAlign,
-      );
+  }) => AmountText(
+    CurrencyFormatter.fromDouble(amount),
+    key: key,
+    fontSize: fontSize,
+    color: color,
+    fontWeight: fontWeight,
+    textAlign: textAlign,
+    amount: amount,
+  );
 
   /// Factory for integer values
   factory AmountText.fromInt(
@@ -43,15 +45,15 @@ class AmountText extends StatelessWidget {
     FontWeight fontWeight = FontWeight.bold,
     TextAlign? textAlign,
     Key? key,
-  }) =>
-      AmountText(
-        CurrencyFormatter.fromDouble(amount.toDouble()),
-        key: key,
-        fontSize: fontSize,
-        color: color,
-        fontWeight: fontWeight,
-        textAlign: textAlign,
-      );
+  }) => AmountText(
+    CurrencyFormatter.fromDouble(amount.toDouble()),
+    key: key,
+    fontSize: fontSize,
+    color: color,
+    fontWeight: fontWeight,
+    textAlign: textAlign,
+    amount: amount.toDouble(),
+  );
 
   /// Factory for cent values
   factory AmountText.fromCents(
@@ -61,33 +63,44 @@ class AmountText extends StatelessWidget {
     FontWeight fontWeight = FontWeight.bold,
     TextAlign? textAlign,
     Key? key,
-  }) =>
-      AmountText(
-        CurrencyFormatter.fromCents(amount),
-        key: key,
-        fontSize: fontSize,
-        color: color,
-        fontWeight: fontWeight,
-        textAlign: textAlign,
-      );
+  }) => AmountText(
+    CurrencyFormatter.fromCents(amount),
+    key: key,
+    fontSize: fontSize,
+    color: color,
+    fontWeight: fontWeight,
+    textAlign: textAlign,
+    amount: amount / 100, // Normalize cents to double for comparison
+  );
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    
+
+    // Logic for conditional coloring
+    Color? effectiveColor = color;
+
+    if (effectiveColor == null && amount != null) {
+      if (amount! > 0) {
+        effectiveColor = Colors.green; // Success
+      } else if (amount! < 0) {
+        effectiveColor = Colors.red; // Danger
+      } else {
+        effectiveColor = Colors.black; // Neutral zero
+      }
+    }
+
     return FittedBox(
       fit: BoxFit.scaleDown,
-      // Ensures the scaling stays consistent with your intended alignment
       alignment: textAlign == TextAlign.center ? Alignment.center : Alignment.centerLeft,
       child: Text(
         text,
         textAlign: textAlign,
         style: TextStyle(
-          color: color ?? theme.textTheme.bodyLarge?.color,
+          color: effectiveColor ?? theme.textTheme.bodyLarge?.color,
           fontSize: fontSize,
           fontWeight: fontWeight,
-          // Mandated for numeric data in your design system
-          fontFamily: 'NotoSansMono', 
+          fontFamily: 'NotoSansMono',
         ),
       ),
     );
